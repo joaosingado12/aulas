b14bca91faa5e9c8c668a61f225cb1e0f870b6f3
joao_costa <110484414+joaosingado12@users.noreply.github.com>
2025-05-19 16:55:52 -0300

    css pronto

---
+++ REACT/react-project/pages/Login/index.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+function Login() {
+    return (
+        <div>
+            <h2>Login</h2>
+        </div>
+    )
+}
+export default Login;
--- REACT/react-project/src/App.jsx
+++ REACT/react-project/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Cadastro from "../pages/Cadastro/index.jsx"
+import Login from "../pages/Login/index.jsx"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Cadastro />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
